13c74fa0c74b16756a733764dbe57fb15a376aab
rakib-utsho <rakibutsho1920@gmail.com>
2026-05-22 18:41:47 +0600

    Implement database connection and load environment variables

---
+++ internal/database/database.go
@@ -0,0 +1,33 @@
+package Database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DB *pgxpool.Pool
+
+func ConnectDB() error {
+	dbURL := os.Getenv("DATABASE_URL")
+
+	db, err := pgxpool.New(context.Background(), dbURL)
+
+	if err != nil {
+		return err
+	}
+
+	err = db.Ping(context.Background())
+
+	if err != nil {
+		return err
+	}
+
+	DB = db
+
+	fmt.Println("Database connected successfully")
+
+	return nil
+}
--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
+
+	Database "money-manager-server/internal/database"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	err = Database.ConnectDB()
+
+	if err != nil {
+		log.Fatal("Database connection failed", err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Money Manager API Running")
 	})
